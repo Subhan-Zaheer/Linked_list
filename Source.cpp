8e1cd74e3879394406dcccef3a2ab4ef71ba4f16
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-07-07 00:15:59 +0500

    Delete by Value function added in linked list

--- Source.cpp
+++ Source.cpp
@@ -20,8 +20,45 @@ public:
 	int insertatHead(int num);
 	int insert(int num);
 	int insertat_Index(int index, int num);
+	int delete_byval(int num);
 	void display();
 };
+int linkList::delete_byval(int num) {
+
+	node* temp = head;
+	bool n = false;
+	if (temp == NULL) {
+
+		cout << "\nLink list is empty." << endl;
+		return 0;
+	}
+	else {
+
+			if (head->data == num) {
+				node* anotherTemp = head;
+				head = head->next;
+				delete anotherTemp;
+				n = true;
+				return 1;
+			}
+		while (temp->next != NULL) {
+
+			if (temp->next->data == num) {
+				node* anotherTemp = NULL;
+				anotherTemp = temp->next;
+				temp->next = temp->next->next;
+				n = true;
+				delete anotherTemp;
+				return 1;
+			}
+			temp = temp->next;
+		}
+		if (n == false) {
+			cout << "Value did not found." << endl;
+			return 0;
+		}
+	}
+}
 int linkList::insertat_Index(int index, int num) {
 	int counter = 0;
 	node* newNode = new node;
@@ -89,9 +126,14 @@ int main() {
 	cout << endl;
 	l1.insertatHead(0);
 	l1.display();
+	cout << endl;
 	l1.insertat_Index(2, 2);
 	l1.display();
-	
+	cout << endl;
+	l1.delete_byval(6);
+	l1.display();
+	cout << endl;
+
 	return 0;
 }
 
